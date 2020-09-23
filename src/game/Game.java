0422a52c99c9e384e9b7a3c3414a8574c89d6173
sylwiasnioch <s>
2020-09-23 12:18:24 +0200

    We made a recursive loop between turn 1 and turn 2

--- src/game/Game.java
+++ src/game/Game.java
@@ -13,14 +13,57 @@ public class Game {
     public Game()
     {
         lol=4;
+        dice = new DiceCup();
     }
 
-    public void turn()
+    public void turn1()
     {
+        int point;
+        dice.rollDice();
+        System.out.println(dice.getDice1());
+        System.out.println(dice.getDice2());
+
         /*
         player1 roll dice
-        dice.rollDice()
+        dice.rollDice();
+        point = dice.getSum();
+        player1.addPoint(point);
+        System.out.println("player1 points");
+        System.out.println(player1.getPoints());
+        if (player1.getPoints() > 40)
+        {
+            System.out.println("game over");
+            return;
+        }
+        else
+        {
+            turn2();
+        }
+         */
+    }
+    public void turn2()
+    {
+        int point;
+        dice.rollDice();
+        System.out.println(dice.getDice1());
+        System.out.println(dice.getDice2());
 
+        /*
+        player1 roll dice
+        dice.rollDice();
+        point = dice.getSum();
+        player2.addPoint(point);
+        System.out.println("player2 points");
+        System.out.println(player2.getPoints());
+        if (player2.getPoints() > 40)
+        {
+            System.out.println("game over");
+            return;
+        }
+        else
+        {
+            turn1();
+        }
          */
     }
 
