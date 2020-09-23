043e35643c9c46a88f2ea1e88ff53a60fcc464c5
sylwiasnioch <s>
2020-09-23 11:32:24 +0200

    We have created player1, player 2 and dice in game variables

--- src/game/Game.java
+++ src/game/Game.java
@@ -1,4 +1,27 @@
 package game;
 
+import dice.DiceCup;
+import player.Player;
+
 public class Game {
+    public int lol;
+
+    private Player player1;
+    private Player player2;
+    private DiceCup dice;
+
+    public Game()
+    {
+        lol=4;
+    }
+
+    public void turn()
+    {
+        /*
+        player1 roll dice
+        dice.rollDice()
+
+         */
+    }
+
 }
