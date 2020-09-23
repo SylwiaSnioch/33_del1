eed63eba8e02eda75f44bf31f8b4c071ba6b9274
PeliPeli2 <harisinavi@gmail.com>
2020-09-23 11:50:38 +0200

    Implementerede funktionerne

--- src/player/Player.java
+++ src/player/Player.java
@@ -7,5 +7,23 @@ public class Player {
     public int getPoint() {
         return point;
     }
+    public void resetPoint() {
+        point = 0;
+    }
+    public void addPoint(int point) {
+        this.point += point;
 
+    }
+    public int getlastdice1() {
+        return lastdice1;
+    }
+    public int getLastdice2() {
+        return lastdice2;
+    }
+    public void setlastdice1(int eyes) {
+        lastdice1 = eyes;
+    }
+    public void setlastdice2(int eyes) {
+        lastdice2 = eyes;
+    }
 }
