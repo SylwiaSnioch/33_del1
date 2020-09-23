6dfd8e2e2704e5c0b640bde4745481cb6c963775
Marcus L. Jensen <supermand3@live.dk>
2020-09-23 11:26:09 +0200

    DiceCup version 1.0 - Marcus og Hassan

--- src/dice/DiceCup.java
+++ src/dice/DiceCup.java
@@ -35,6 +35,7 @@ public class DiceCup {
         for (int i = 0; i < 1001; i++) {
             rollDice();
             System.out.println("You rolled " + dice1 + " and " + dice2);
+            System.out.println("test");
         }
     }
 
