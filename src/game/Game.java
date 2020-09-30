4c7ce0c6e92db427e9a4170135db87ebd3a0830d
Markus Thieden <mit@patentrenewal.com>
2020-09-30 11:06:11 +0200

    added the rules from task 3

--- src/game/Game.java
+++ src/game/Game.java
@@ -2,8 +2,6 @@ package game;
 
 import dice.DiceCup;
 import player.Player;
-
-import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Game {
@@ -37,6 +35,20 @@ public class Game {
             player1.addPoint(point);
             System.out.println("player1 points");
             System.out.println(player1.getPoint());
+
+            // player wins if they roll two sixes two turns in a row
+            if (dice.getDice1() == 6 && dice.getDice2() == 6 && player1.getlastdice1() == 6 && player1.getLastdice2() == 6)
+            {
+                System.out.println("game over");
+                System.out.println(player1.getName() + " has won the game");
+                return;
+            }
+            else
+            {
+                player1.setlastdice1(dice.getDice1());
+                player1.setlastdice2(dice.getDice2());
+            }
+
             if (player1.getPoint() > 40)
             {
                 System.out.println("game over");
@@ -72,6 +84,20 @@ public class Game {
             player2.addPoint(point);
             System.out.println("player2 points");
             System.out.println(player2.getPoint());
+
+            // player wins if they roll two sixes two turns in a row
+            if (dice.getDice1() == 6 && dice.getDice2() == 6 && player2.getlastdice1() == 6 && player2.getLastdice2() == 6)
+            {
+                System.out.println("game over");
+                System.out.println(player2.getName() + " has won the game");
+                return;
+            }
+            else
+            {
+                player2.setlastdice1(dice.getDice1());
+                player2.setlastdice2(dice.getDice2());
+            }
+
             if (player2.getPoint() > 40)
             {
                 System.out.println("game over");
